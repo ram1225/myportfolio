f9b6e88c5910fcf51e8639c778a96a6f28d52a6e
Ramakrishna <ramakrishna.erroju@siemens.com>
2019-08-29 15:26:44 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,19 @@ import React from 'react';
 
 export default function About(){
     return (
-        <div>
-            <h3 className="paddingLeft">About</h3>
+        <div className="about-container home-container">
+            <h3 className="text-center">About</h3>
+            <div>
+                <p><strong>Hey There! </strong>I'm Ramakrishna, I'm a self made software developer</p>
+                <p>I extensively work on the hybrid mobile app area using ionic, react native</p>
+                <p>Also work on building interactive, efficient web applications on different domains</p>
+                <p>And I have solid expertise on Native Android application developement also.</p>
+                <div>
+                    <p>When I do not work on above stuff, you can find me hitting sixes over the fence,
+                        cooking some tasty food and watching out movies with coffee mug in hand.                       
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
